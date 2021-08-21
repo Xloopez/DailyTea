85b3533e08db2eed41fa9f0e962d8090daae14a5
nicolelopezfeliz <nicole.lopez@live.se>
2021-08-21 12:14:11 +0200

    Created header

--- App.js
+++ App.js
@@ -2,20 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/Header';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header />
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
