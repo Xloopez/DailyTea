f41982eea2e44df775ed4eca5fe941de1bae6fcd
nicolelopezfeliz <nicole.lopez@live.se>
2021-08-25 15:32:29 +0200

    Added drawer navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import NewsListScreen from '../screens/NewsListScreen';
 import NewsDetailScreen from '../screens/NewdsDetailScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
+import AboutScreen from '../screens/AboutScreen';
 
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function HomeNavigator() {
     return (
@@ -30,30 +33,47 @@ function HomeNavigator() {
 }
 
 function FavoriteNavigator() {
-    return(
+    return (
         <Stack.Navigator>
             <Stack.Screen name="Favorites" component={FavoriteScreen} />
         </Stack.Navigator>
     );
 }
 
+function aboutNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="About" component={AboutScreen} />
+        </Stack.Navigator>
+    );
+}
+
+function TabsNavigator() {
+    return (
+        <Tabs.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: () => {
+                let iconName;
+                if (route.name == "Home") {
+                    iconName = "home"
+                } else if (route.name == "Favorite") {
+                    iconName = 'favorite'
+                }
+                return <MaterialIcons name={iconName} color="#111a27" size={24} />
+            }
+        })}>
+            <Tabs.Screen name="Home" component={HomeNavigator} />
+            <Tabs.Screen name="Favorite" component={FavoriteNavigator} />
+        </Tabs.Navigator>
+    );
+}
+
 function AppNavigator() {
     return (
         <NavigationContainer>
-            <Tabs.Navigator screenOptions={({route}) => ({
-                tabBarIcon: () => {
-                    let iconName;
-                    if(route.name == "Home") {
-                        iconName = "home"
-                    } else if (route.name == "Favorite"){
-                        iconName= 'favorite'
-                    }
-                    return <MaterialIcons name={iconName} color="#111a27" size={24} /> 
-                }
-            })}>
-                <Tabs.Screen name="Home" component={HomeNavigator} />
-                <Tabs.Screen name="Favorite" component={FavoriteNavigator} /> 
-            </Tabs.Navigator>
+            <Drawer.Navigator>
+                <Drawer.Screen name="News" component={TabsNavigator} />
+                <Drawer.Screen name="About" component={AboutScreen} />
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }
